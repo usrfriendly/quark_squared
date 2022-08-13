e338a224cee1d6741e7b380f53f7fbdf22172efb
usrfriendly <arinlares@gmail.com>
2022-08-12 20:27:29 -0700

    Set up all major keysets

--- keymap.c
+++ keymap.c
@@ -16,6 +16,10 @@
 
 #include QMK_KEYBOARD_H
 
+// Vi-Keys on home row with hold
+#define EVIM LT(4,KC_ESC)
+#define CNUM LCTL_T(TG(5))
+
 // Defines for task manager and such
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -32,7 +36,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |   OS   |   Alt   |         Layer        |      Space & Layer     |     [    |    ]      |
      * `-----------------------------------------------------------------------------------------'
      */
-	[0] = LAYOUT_5_2u(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_DOT, KC_DOT, KC_SLSH, KC_ENT, KC_LCTL, KC_LGUI, KC_LALT, MO(1), KC_SPC, MO(2), KC_RALT, KC_APP, KC_RCTL),
+	[0] = LAYOUT_5_2u(
+	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+	KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+	EVIM,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+	KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+	CNUM,     KC_LGUI,     KC_LALT,     MO(1),   KC_SPC,           MO(2),     KC_RALT,     KC_APP,     KC_RCTL
+	),
 
     /* [1]
      * ,---------------------------------------------------------------------------------------.
@@ -47,7 +57,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |    ESC  |    CTRL-ALT-DEL        |             |           |      '|'     |    `      |
      * `---------------------------------------------------------------------------------------'
      */
-	[1] = LAYOUT_5_2u(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_INS, KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC, KC_CAPS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_BSLS, KC_TRNS, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_HOME, KC_END, KC_MPLY, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(3), KC_VOLD, KC_VOLU, KC_MFFD),
+	[1] = LAYOUT_5_2u(
+	KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_INS,
+	KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+	KC_CAPS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_BSLS,
+	KC_TRNS, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_HOME, KC_END, KC_MPLY,
+	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(5), MO(3), KC_VOLD, KC_VOLU, KC_MNXT),
 
     /* [2]
      * ,---------------------------------------------------------------------------------------.
@@ -62,7 +77,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |     RESET    |              |             |            |                |            |
      * `---------------------------------------------------------------------------------------'
      */
-	[2] = LAYOUT_5_2u(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_INS, KC_NO, KC_NO, KC_3, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NUM, KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_LSFT, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO, KC_NO, KC_PGDN, KC_PGUP, KC_ENT, KC_TRNS, KC_TRNS, KC_TRNS, MO(3), KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+	[2] = LAYOUT_5_2u(
+	KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_INS,
+	_______, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, _______,
+	_______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,
+	KC_LSFT, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, _______, KC_PGDN, KC_PGUP, KC_ENT,
+	KC_TRNS, KC_TRNS, KC_TRNS, MO(3), _______, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
 
     /* [3]
@@ -78,7 +98,53 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |     RESET    |              |             |            |                |            |
      * `---------------------------------------------------------------------------------------'
      */
-	[3] = LAYOUT_5_2u(QK_BOOT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO)
+	[3] = LAYOUT_5_2u(
+	RESET, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+	_______, _______, _______, KC_TRNS, _______, KC_TRNS, _______, _______, _______),
+    
+     /* [4]
+     * ,---------------------------------------------------------------------------------------.
+     * |   =    |   1  |   2  |   3  |   4  |   5  |  6  |   7  |   8  |   9  |   0   |   -   |
+     * |------------+------+------+------+------+------+------+------+------+------+------+---|
+     * |       |      |      |  UP  |       |      |     |   _   |     |   [   |  ]    |      |
+     * |------------+------+------+------+------+-------------+------+------+------+------+---|
+     * |       |     | LEFT | DOWN  | RIGHT |     |      |   -  |      |   [   |   ]   |      |
+     * |------------+------+------+------+------+-----+-----+------+------+------+------+-----|
+     * |   F1  | F2 |  F3  |   F4  | F5    | F6  | F7   | F8   | F9    | F10  | F11   | F12   |
+     * |-------+-------+-------+-------+-------+-------+------+------+------+------+------+---|
+     * |     RESET    |              |             |            |                |            |
+     * `---------------------------------------------------------------------------------------'
+     */
+	[4] = LAYOUT_5_2u(
+	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+	_______, _______, _______, _______, _______, _______, _______, _______, KC_UP, _______, _______, _______,
+	_______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_LEFT, _______, _______,
+	_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+	_______, _______, _______, _______, _______, _______, _______, _______, _______),
+
+
+     /* [5]
+     * ,---------------------------------------------------------------------------------------.
+     * |   =    |   1  |   2  |   3  |   4  |   5  |  6  |   7  |   8  |   9  |   0   |   -   |
+     * |------------+------+------+------+------+------+------+------+------+------+------+---|
+     * |       |      |      |  UP  |       |      |     |   _   |     |   [   |  ]    |      |
+     * |------------+------+------+------+------+-------------+------+------+------+------+---|
+     * |       |     | LEFT | DOWN  | RIGHT |     |      |   -  |      |   [   |   ]   |      |
+     * |------------+------+------+------+------+-----+-----+------+------+------+------+-----|
+     * |   F1  | F2 |  F3  |   F4  | F5    | F6  | F7   | F8   | F9    | F10  | F11   | F12   |
+     * |-------+-------+-------+-------+-------+-------+------+------+------+------+------+---|
+     * |     RESET    |              |             |            |                |            |
+     * `---------------------------------------------------------------------------------------'
+     */
+	[5] = LAYOUT_5_2u(
+	_______, _______, _______, _______, _______, _______, _______, KC_NUM, KC_PSLS, KC_PMNS, KC_PMNS, _______,
+	_______, _______, _______, _______, _______, _______, _______, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, _______,
+	_______, _______, _______, _______, _______, _______, _______, KC_P4,   KC_P5,   KC_P6,   KC_PENT, _______,
+	_______, _______, _______, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,   _______, _______,
+	_______,    _______,    _______,    _______, _______,          KC_P0,    _______,    _______,    _______)
 
 };
 
