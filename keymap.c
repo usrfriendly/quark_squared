13648b14609a7dcb48d96d5dd423c91e3a04f743
usrfriendly <arinlares@gmail.com>
2022-09-01 14:22:12 -0700

    Finished binding media controls for symmetry

--- keymap.c
+++ keymap.c
@@ -58,9 +58,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `---------------------------------------------------------------------------------------'
      */
 	[1] = LAYOUT_5_2u(
-	KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_INS,
+	KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_INS,
 	KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-	KC_CAPS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_BSLS,
+	KC_CAPS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
 	KC_TRNS, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_TRNS, KC_HOME, KC_END, KC_MPLY,
 	KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, MO(5), MO(3), KC_VOLD, KC_VOLU, KC_MNXT),
 
@@ -81,8 +81,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_INS,
 	_______, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, _______,
 	_______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,
-	KC_LSFT, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, _______, KC_PGDN, KC_PGUP, KC_ENT,
-	KC_TRNS, KC_TRNS, KC_TRNS, MO(3), _______, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+	KC_LSFT, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, _______, KC_PGDN, KC_PGUP, KC_MPLY,
+	KC_TRNS, KC_TRNS, KC_TRNS, MO(3), _______, KC_TRNS, KC_VOLD, KC_VOLU, KC_MNXT),
 
 
     /* [3]
